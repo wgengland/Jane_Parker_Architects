0cf9a8a14e33a4d90c16ac93f1275197ef6bacb3
George England <jlparker@Georges-MacBook-Air-5.local>
2020-04-13 22:40:26 +0100

    Working!

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const slider = document.querySelector('.slider');
 const logo = document.querySelector('#logo');
 const waves = document.querySelector('.waves');
 const headline = document.querySelector('.headline');
-
+console.log("hello world");
 const tl = new TimelineMax();
-tl.fromTo(wallpaper,1,{height:"0%"},{height:"80%",ease: Power2.easeInOut}).fromTo(wallpaper,1.2,{width:"100%"},{width:"80%",ease:Power2.easeInOut}).fromTo(slider,1.2,{x:"-100%"},{x:"0%",ease:Power2.easeInOut},"-=1.2");
+tl.fromTo(wallpaper,1,{height:"0%"},{height:"100%",ease: Power2.easeInOut}).fromTo(wallpaper,1.2,{width:"100%"},{width:"100%",ease:Power2.easeInOut}).fromTo(slider,1.2,{x:"-100%"},{x:"0%",ease:Power2.easeInOut},"-=1.2");
